97cee6992ce61dff4fa06916667f433fe9ddc6c8
lubuoren <86152256+lubuoren@users.noreply.github.com>
2022-03-13 07:54:37 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 let list: number[] = []
-pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
-    list = [
-    pins.digitalReadPin(DigitalPin.P1),
-    pins.digitalReadPin(DigitalPin.P2),
-    pins.digitalReadPin(DigitalPin.P3),
-    pins.digitalReadPin(DigitalPin.P4),
-    pins.digitalReadPin(DigitalPin.P5),
-    pins.digitalReadPin(DigitalPin.P6),
-    pins.digitalReadPin(DigitalPin.P7),
-    pins.digitalReadPin(DigitalPin.P8)
-    ]
-})
 basic.forever(function () {
-	
+    if (pins.analogReadPin(AnalogPin.P0) == 1) {
+        list = [
+        pins.digitalReadPin(DigitalPin.P1),
+        pins.digitalReadPin(DigitalPin.P2),
+        pins.digitalReadPin(DigitalPin.P3),
+        pins.digitalReadPin(DigitalPin.P4),
+        pins.digitalReadPin(DigitalPin.P5),
+        pins.digitalReadPin(DigitalPin.P6),
+        pins.digitalReadPin(DigitalPin.P7),
+        pins.digitalReadPin(DigitalPin.P8),
+        pins.digitalReadPin(DigitalPin.P9),
+        pins.digitalReadPin(DigitalPin.P10),
+        pins.digitalReadPin(DigitalPin.P0)
+        ]
+    }
 })
